8097039019dd711737242fe40af6961190b84a10
m1lkyway123 <124919438+m1lkyway123@users.noreply.github.com>
2023-03-29 16:37:56 +0300

    added a responsive section

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,6 +8,23 @@ $(document).ready(function(){
     });
 });
 
+$(document).ready(function(){
+    $('.consectet__inner').slick({
+        infinite: false,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: false,
+        responsive: [
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 2,
+              }
+            }
+        ]
+    });
+});
+
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("burger").addEventListener("click", function() {
         document.querySelector("header").classList.toggle("open");
