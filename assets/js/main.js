e7f0c157557f32ad55ec39570cac9a76131f3779
m1lkyway123 <124919438+m1lkyway123@users.noreply.github.com>
2023-04-02 11:17:28 +0300

    added a responsive footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -44,5 +44,6 @@ $(document).ready(function(){
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("burger").addEventListener("click", function() {
         document.querySelector("header").classList.toggle("open");
+        document.querySelector("body").classList.toggle("open");
     })
 })
